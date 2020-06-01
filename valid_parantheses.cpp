f10bb73107dd39e87ae992978ef91ad1dbea8b40
Michael Ganzorig <manlai1025@gmail.com>
2020-06-01 02:10:19 -0400

    solution to valid parantheses

---
+++ valid_parantheses.cpp
@@ -0,0 +1,32 @@
+#include <stack>
+#include <string>
+
+class Solution
+{
+public:
+    bool isValid(std::string s)
+    {
+        std::stack<char> p;
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(s[i] == '{' || s[i] == '[' || s[i] == '(')
+                p.push(s[i]);
+            else if(p.empty())
+                return false;
+            else if(s[i] == '}' && p.top() != '{')
+                return false;
+            else if(s[i] == ')' && p.top() != '(')
+                return false;
+            else if(s[i] == ']' && p.top() != '[')
+                return false;
+            else
+                p.pop();
+        }
+        return p.empty();
+    }
+};
+
+int main()
+{
+
+}
