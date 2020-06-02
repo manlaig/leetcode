1b7721cc69194894987ce697ca1e4de408d4976d
Michael Ganzorig <manlai1025@gmail.com>
2020-06-02 00:11:21 -0400

    solution

---
+++ sqrt_x.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+
+/*
+    Maybe it wanted me to use some math formula to compute sqrt,
+    but I just used a library function
+*/
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        return sqrt(x);
+    }
+};
+
+int main()
+{
+
+}
