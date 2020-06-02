7e7b71c0cd0323602af40f3342f567aef8db9211
Michael Ganzorig <manlai1025@gmail.com>
2020-06-02 01:51:30 -0400

    solution to permutations problem

--- permutations.cpp
+++ permutations.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
-#include <set>
+#include <vector>
 
-void permute_helper(std::set<std::string>* out, std::string str, int start, int end)
+void helper(std::vector<std::vector<int>>& out, std::vector<int> arr, int start, int end)
 {
     if(start >= end)
+    {
+        out.push_back(arr);
         return;
-    out->insert(str);
+    }
     for(int i = start; i < end; i++)
     {
-        std::swap(str[start], str[i]);
-        permute_helper(out, str, start+1, end);
+        std::swap(arr[i], arr[start]);
+        helper(out, arr, start+1, end);
+        std::swap(arr[i], arr[start]);
     }
 }
 
-std::set<std::string>* permutations(const std::string& str)
+std::vector<std::vector<int>> permute(std::vector<int>& nums)
 {
-    std::set<std::string>* v = new std::set<std::string>();
-    permute_helper(v, str, 0, str.size());
-    return v;
+    std::vector<std::vector<int>> out;
+    helper(out, nums, 0, nums.size());
+    return out;
 }
 
 int main()
