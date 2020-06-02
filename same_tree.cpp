f438273c6b72fc25480dc21c0f391a9f2cd2277b
Michael Ganzorig <manlai1025@gmail.com>
2020-06-02 14:19:38 -0400

    solution

---
+++ same_tree.cpp
@@ -0,0 +1,24 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+bool isSameTree(TreeNode* p, TreeNode* q)
+{
+    if(!p && !q)
+        return true;
+    if(!p || !q)
+        return false;
+    bool left = isSameTree(p->left, q->left);
+    bool right = isSameTree(p->right, q->right);
+    return left && right && p->val == q->val;
+}
+
+int main()
+{
+    
+}
