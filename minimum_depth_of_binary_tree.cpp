028ec3cc2d11527204f1f8df008ca1f277f01792
Michael Ganzorig <manlai1025@gmail.com>
2020-06-02 14:46:52 -0400

    solution

---
+++ minimum_depth_of_binary_tree.cpp
@@ -0,0 +1,23 @@
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+int minDepth(TreeNode* root)
+{
+    if(!root)
+        return 0;
+    if(!root->left || !root->right)
+        return minDepth(root->left ? root->left : root->right) + 1;
+    return std::min(minDepth(root->left), minDepth(root->right)) + 1;
+}
+
+int main()
+{
+    
+}
