2e901dc569540c64a94806b3d163b9698ad29a9e
Michael Ganzorig <manlai1025@gmail.com>
2020-06-03 19:26:17 -0400

    explanation of solution

--- move_zeroes.cpp
+++ move_zeroes.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 #include <vector>
 
+/*
+    This is same as the technique called "Two Pointers" by LeetCode
+    [1,0,0,1,3]       ->      [1,1,0,0,3]       ->      [1,1,3,0,0]
+       ^   i                         ^   i                       ^ i
+*/
+
 void moveZeroes(std::vector<int>& nums)
 {
     for(int i = 0, next = 0; i < nums.size(); i++)
     {
+        // increment next until we find a 0
         if(nums[next] != 0)
             next++;
         else if(nums[i])
         {
+            // we know nums[next] == 0
             std::swap(nums[next], nums[i]);
             next++;
         }
