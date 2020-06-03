326b9d8e1e76870ea3c6c9649e646e6ea846b545
Michael Ganzorig <manlai1025@gmail.com>
2020-06-02 22:39:01 -0400

    kadane's algorithm for maximum subarray

--- maximum_subarray.cpp
+++ maximum_subarray.cpp
@@ -3,7 +3,35 @@
 #include <map>
 using namespace std;
 
-int maxSubArray(vector<int>& nums) {
+// O(n) Kadane's algorithm
+int maxSubArray(vector<int>& nums)
+{
+    /*
+        [-2,1,-3,4,-1,2,1,-5,4]
+        
+        i = 0: [-2]                 = -2
+        i = 1: [1]                  = 1
+        i = 2: [1, -3]              = -2
+        i = 3: [4]                  = 4
+        i = 4: [4, -1]              = 3
+        i = 5: [4, -1, 2]           = 5
+        i = 6: [4, -1, 2, 1]        = 6     this is the answer
+        i = 7: [4, -1, 2, 1, -5]    = 1
+        i = 9: [4, -1, 2, 1, -5, 4] = 5
+    */
+    int max = -2147483648;
+    int buf = 0;
+    for(int i = 0; i < nums.size(); i++)
+    {
+        buf = nums[i] > nums[i] + buf ? nums[i] : nums[i] + buf;
+        if(buf > max)
+            max = buf;
+    }
+    return max;
+}
+
+int maxSubArra_DP(vector<int>& nums)
+{
     map<pair<int, int>, int> sums;
     
     int largest = -2147483648;
