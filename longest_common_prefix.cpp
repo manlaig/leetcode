80d4ba29785abecf41ba28f9c017a8b01378e76e
Michael Ganzorig <manlai1025@gmail.com>
2020-06-03 01:39:31 -0400

    a much faster solution

--- longest_common_prefix.cpp
+++ longest_common_prefix.cpp
@@ -3,52 +3,34 @@
 #include <vector>
 #include <map>
 
-/*
-    NOTE: this code was actually judges as a SLOW solution
-    From the beginning, I thought of prefix to be same as a substring
-    If I had known prefixes correctly, I would've approached the problem differently
-*/
-
-bool all_ones(std::vector<int> v)
-{
-    for(int i : v)
-        if(i != 1)
-            return false;
-    return true;
-}
-
 std::string longestCommonPrefix(std::vector<std::string>& strs)
 {
-    std::map<std::string, std::vector<int> > occur;
+    if(!strs.size())
+        return "";
     
-    for(int i = 0; i < strs.size(); i++)
+    bool cont = true;
+    std::string prefix = "";
+    while(cont)
     {
-        for(int end = 0; end < strs[i].size(); end++)
+        if(strs[0].size() <= prefix.size())
+            break;
+        char s = strs[0][prefix.size()];
+        for(int i = 1; i < strs.size(); i++)
         {
-            std::string temp = strs[i].substr(0, end + 1);
-            if(occur.find(temp) == occur.end())
+            if(strs[i].size() < prefix.size() || strs[i][prefix.size()] != s)
             {
-                std::vector<int> v(strs.size());
-                v[i] = 1;
-                occur[temp] = v;
-            } else
-            {
-                occur[temp][i] = 1;
+                cont = false;
+                break;
             }
         }
+        if(cont)
+            prefix += s;
     }
-    
-    std::string best = "";
-    for(std::pair<std::string, std::vector<int> > p : occur)
-    {
-        if(all_ones(p.second) && p.first.size() > best.size())
-            best = p.first;
-    }
-    return best;
+    return prefix;
 }
 
 int main()
 {
-    std::vector<std::string> v = {"doga", "car", "racecar"};
+    std::vector<std::string> v = {"doga", "dog", "dogracecar"};
     std::cout << longestCommonPrefix(v) << std::endl;
 }
