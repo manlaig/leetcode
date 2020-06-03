b0ea70b02c94d13458d02ee3f61d1e0a4272c28e
Michael Ganzorig <manlai1025@gmail.com>
2020-06-03 01:52:19 -0400

    explanation of solution

--- delete_node_in_a_linked_list.cpp
+++ delete_node_in_a_linked_list.cpp
@@ -7,6 +7,20 @@ struct ListNode
 
 void deleteNode(ListNode* node)
 {
+    /*
+        we aren't given the head, plus the pointer's not passed by reference
+        and the function returns void, so we have "propagate" the values
+        
+                delete 4
+                    V
+        1->2->3->4->5->6->7
+            loop condition stops here
+                        V    
+        1->2->3->5->6->6->7
+            the outer loop copies the last value and deletes the last
+        1->2->3->5->6->7
+        
+    */
     while(node->next->next)
     {
         node->val = node->next->val;
