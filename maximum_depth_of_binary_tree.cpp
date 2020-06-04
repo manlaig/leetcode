6c3c9e36bc5fc99f05985a9640ac5c2335bbf742
Michael Ganzorig <manlai1025@gmail.com>
2020-06-04 19:16:19 -0400

    solution

--- maximum_depth_of_binary_tree.cpp
+++ maximum_depth_of_binary_tree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stack>
 
 struct TreeNode
 {
@@ -15,11 +16,33 @@ struct TreeNode
     I'm thinking of making it iterative
 */
 
-int maxDepth(TreeNode* root, int depth = 0)
+int maxDepth_recur(TreeNode* root, int depth = 0)
 {
     if(!root)
         return depth;
-    return std::max(maxDepth(root->left, depth + 1), maxDepth(root->right, depth + 1));
+    return std::max(maxDepth_recur(root->left, depth + 1), maxDepth_recur(root->right, depth + 1));
+}
+
+int maxDepth_iter(TreeNode* root, int depth = 0)
+{
+    int max = 0;
+    std::stack<std::pair<TreeNode*, int> > s;
+    if(root)
+        s.push(std::make_pair(root, 1));
+    
+    while(!s.empty())
+    {
+        std::pair<TreeNode*,int> p = s.top();
+        s.pop();
+        
+        if(p.first->left)
+            s.push(std::make_pair(p.first->left, p.second + 1));
+        if(p.first->right)
+            s.push(std::make_pair(p.first->right, p.second + 1));
+        if(!p.first->left && !p.first->right && p.second > max)
+            max = p.second;
+    }
+    return max;
 }
 
 int main()
