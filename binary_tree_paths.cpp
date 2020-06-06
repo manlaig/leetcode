85b457dfa39ca38087b23103a6e2403e0559197d
Michael Ganzorig <manlai1025@gmail.com>
2020-06-06 16:43:48 -0400

    DFS solution

--- binary_tree_paths.cpp
+++ binary_tree_paths.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <string>
+#include <stack>
 using namespace std;
 
 struct TreeNode
@@ -25,14 +26,41 @@ void helper(TreeNode* root, vector<string>& out, string curr)
     helper(root->right, out, curr + to_string(root->val) + "->");
 }
 
-vector<string> binaryTreePaths(TreeNode* root)
+vector<string> binaryTreePaths_recur(TreeNode* root)
 {
     vector<string> out;
     helper(root, out, "");
     return out;
 }
 
+vector<string> binaryTreePaths(TreeNode* root)
+{
+    vector<string> out;
+    if(!root)
+        return out;
+    stack<pair<TreeNode*, string> > s;
+    s.push(make_pair(root, ""));
+    
+    while(s.size())
+    {
+        pair<TreeNode*, string> top = s.top();
+        s.pop();
+        
+        string edited = top.second + "->" + to_string(top.first->val);
+        if(top.first == root)
+            edited = to_string(top.first->val);
+
+        if(!top.first->left && !top.first->right)
+            out.push_back(edited);
+        if(top.first->left)
+            s.push(make_pair(top.first->left, edited));
+        if(top.first->right)
+            s.push(make_pair(top.first->right, edited));
+    }
+    return out;
+}
+
 int main()
 {
-    
+
 }
