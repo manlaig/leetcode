5057604469f2028840c398e36cb3c723fabd2fa5
Michael Ganzorig <manlai1025@gmail.com>
2020-06-09 15:52:19 -0400

    explanation of solution and complexity

--- diameter_of_binary_tree.cpp
+++ diameter_of_binary_tree.cpp
@@ -10,6 +10,20 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+/*
+    In tree problems, I often come up with recursive solutions. They can be converted to iterative approaches,
+    but recursive solutions are more straightforward to implement. To come up with a recursive solution,
+    it's important to see the problem as many subproblems
+
+    For example, we know the longest may or may not pass through the root node, so that means the longest path is
+    in either left or right branches or it passed through the root. In that case where it passes through,
+    the length of it is the height sum of the left and right branches.
+
+    The complexity of this solution is O(NlogN) time and O(logN) space.
+    We visit each node and get the height at each node, that results in O(NlogN)
+    We allocate O(logN) space because the call stack of the left branch call is destroyed before the right branch call.
+*/
+
 int getHeight(TreeNode* root)
 {
     if(!root)
