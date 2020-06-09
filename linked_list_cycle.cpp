c15913e34fe00b13dd71160da691f56a8a06e4dc
Michael Ganzorig <manlai1025@gmail.com>
2020-06-09 18:52:18 -0400

    explanation

--- linked_list_cycle.cpp
+++ linked_list_cycle.cpp
@@ -21,8 +21,22 @@ bool hasCycle(ListNode *head)
     // then we immediate have a cycle (by how our algorithm works)
     ListNode* fast = head->next;
     ListNode* slow = head;
+
+    // you can also do it like this, but this loop only advances 2 steps in each iteration
+    // so this one can be slower, even though the code is shorter
+    /*
+    while(fast != slow)
+    {
+        if(!fast || !fast->next)
+            return false;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return true;
+    */
     
     // fast is always ahead, fast is null BEFORE slow is null
+    // the reason this loop is faster than the one above is because it "unrolls" the loop
     while(fast)
     {
         // unfortunately, since fast moves multiple times each iteration,
