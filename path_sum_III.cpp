0c09f7e8a6c2fd97f93bb98ecf69c8f7d492614f
Michael Ganzorig <manlai1025@gmail.com>
2020-06-09 18:32:52 -0400

    explanation

--- path_sum_III.cpp
+++ path_sum_III.cpp
@@ -8,11 +8,17 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+/*
+    DFS can also be used to solve this question
+*/
+
+// counts how many paths start from root
 int startPath(TreeNode* root, int sum)
 {
     if(!root)
         return 0;
     
+    // we don't stop we sum = 0, we keep going to find additional paths, that sum to 0
     int total = sum == 0 ? 1 : 0;
     total += startPath(root->left, sum - root->left->val);
     total += startPath(root->right, sum - root->right->val);
