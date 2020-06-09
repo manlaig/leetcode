6bb66142106446e4c5476d33091c596e3ffadefb
Michael Ganzorig <manlai1025@gmail.com>
2020-06-09 16:54:26 -0400

    2 different ways of solving

--- binary_tree_right_side_view.cpp
+++ binary_tree_right_side_view.cpp
@@ -1,3 +1,6 @@
+#include <vector>
+using namespace std;
+
 struct TreeNode
 {
     int val;
@@ -9,7 +12,8 @@ struct TreeNode
 };
 
 /*
-    You can also you DFS for this problem
+    Compile with C++11
+    You can also you DFS for this problem, the DFS solution is below
 */
 
 void helper(TreeNode* root, vector<int>& out, int curr, int& most)
@@ -32,6 +36,36 @@ vector<int> rightSideView(TreeNode* root)
     vector<int> out;
     int most = -1;
     helper(root, out, 0, most);
+    return out;
+}
+
+vector<int> DFS(TreeNode* root)
+{
+    if(!root)
+        return {};
+    
+    vector<int> out;
+    int most = -1;
+    stack<pair<TreeNode*, int>> next;
+    next.push(make_pair(root, 0));
+
+    while(next.size())
+    {
+        auto p = next.top();
+        next.pop();
+
+        TreeNode* curr = p.first;
+        if(p.second > most)
+        {
+            most = p.second;
+            out.push_back(curr->val);
+        }
+
+        if(curr->left)
+            next.push(make_pair(curr->left, p.second+1));
+        if(curr->right)
+            next.push(make_pair(curr->right, p.second+1));
+    }
     return out;
 }
 
