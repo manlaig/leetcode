c758ad9dd517e0c68f5379a1e4d86e72188591e4
Michael Ganzorig <manlai1025@gmail.com>
2020-06-11 12:29:03 -0400

    one pass solution

--- best_time_to_buy_and_sell_stock.cpp
+++ best_time_to_buy_and_sell_stock.cpp
@@ -6,7 +6,7 @@
     It can be either prices[i] OR arr[i-1]
 */
 
-int maxProfit(std::vector<int>& prices)
+int maxProfit(const std::vector<int>& prices)
 {
     if(!prices.size())
         return 0;
@@ -20,6 +20,18 @@ int maxProfit(std::vector<int>& prices)
     for(int i = 0; i < prices.size(); i++)
         if(max < prices[i] - arr[i])
             max = prices[i] - arr[i];
+    return max;
+}
+
+int maxProfit_onepass(const std::vector<int>& prices)
+{
+    int min = prices[0];
+    int max = 0;
+    for(int i = 0; i < prices.size(); i++)
+        if(prices[i] - min > max)
+            max = prices[i] - min;
+        else if(prices[i] < min) 
+            min = prices[i];
     return max;
 }
 
