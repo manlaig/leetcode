19706ddfbeacce91b59f6a8ea40a870dcf988970
Michael Ganzorig <manlai1025@gmail.com>
2020-06-13 21:28:41 -0400

    solution

---
+++ minimum_path_sum.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+
+int minPathSum(const std::vector<std::vector<int>>& grid)
+{
+    if(grid.size() == 0)
+        return 0;
+
+    int m = grid.size();
+    int n = grid[0].size();
+    std::vector<std::vector<int>> dp(m, std::vector<int>(n,1));
+    
+    for(int i = 0; i < m; i++)
+        for(int j = 0; j < n; j++)
+        {
+            dp[i][j] = grid[i][j];
+            // if i = 0 and j = 0, then we'll add INT_MAX to dp[i][j] and we don't want that
+            if(i || j)
+                dp[i][j] += std::min(i-1 >= 0 ? dp[i-1][j] : INT_MAX, j-1 >= 0 ? dp[i][j-1] : INT_MAX);
+        }
+    return dp[m-1][n-1];
+}
+
+int main()
+{
+
+}
