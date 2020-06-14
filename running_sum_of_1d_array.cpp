4fede7a75761d1861bb3c52cf4e222bac521fd96
Michael Ganzorig <manlai1025@gmail.com>
2020-06-14 12:57:22 -0400

    solution

---
+++ running_sum_of_1d_array.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+
+std::vector<int> runningSum(std::vector<int>& nums)
+{
+    for(int i = 1; i < nums.size(); i++)
+        nums[i] += nums[i-1];
+    return nums;
+}
+
+int main()
+{
+
+}
