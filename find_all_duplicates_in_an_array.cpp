bce812cf679292d0aa1a46918620be5d6b9c2abc
Michael Ganzorig <manlai1025@gmail.com>
2020-06-17 01:06:11 -0400

    algorithm walkthrough

--- find_all_duplicates_in_an_array.cpp
+++ find_all_duplicates_in_an_array.cpp
@@ -3,7 +3,11 @@
 /*
     This solution is O(1) space and O(n) time.
     This is a very useful technique for coming up with O(1) space solutions when you are
-    given that the array values are 1 <= arr[i] <= arr.size() or something similar
+    given that the array values are 1 <= arr[i] <= arr.size() or something similar.
+
+    This type of solution "jumps" around the array using the element values (since we know their bounds)
+    Every iteration we negate the element arr[arr[i]]. This tells us whether we visited this element before or not.
+    If arr[i] = 3, then we say arr[3] *= -1; and if arr[3] is already negative, we know we have visited this before.
 */
 
 std::vector<int> findDuplicates(std::vector<int>& nums)
