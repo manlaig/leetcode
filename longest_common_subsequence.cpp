4be1faae9da107fba065940bcf400e28b5e4b8f3
Michael Ganzorig <manlai1025@gmail.com>
2020-06-20 13:02:17 -0400

    solution

--- longest_common_subsequence.cpp
+++ longest_common_subsequence.cpp
@@ -1,45 +1,49 @@
-#include <unordered_map>
 #include <string>
+#include <vector>
 using namespace std;
 
-struct hash_pair 
-{ 
-    template <class T1, class T2> 
-    size_t operator()(const pair<T1, T2>& p) const
-    { 
-        auto hash1 = hash<T1>{}(p.first); 
-        auto hash2 = hash<T2>{}(p.second); 
-        return hash1 ^ hash2; 
-    } 
-}; 
-
-// Time limit exceeded on this recursive solution
-int helper(const string& s1, const string& s2, int i1, int i2,
-            unordered_map<pair<int,int>, int, hash_pair>& m)
+int helper(const string& s1, const string& s2, int i1, int i2, vector<vector<int>>& m)
 {
     if(i1 == s1.size() || i2 == s2.size())
         return 0;
     
-    auto p = make_pair(i1,i2);
-    if(m.find(p) != m.cend())
-        return m[p];
+    if(m[i1][i2])
+        return m[i1][i2];
     
     if(s1[i1] == s2[i2])
-        m[p] = 1 + helper(s1, s2, i1+1, i2+1, m);
+        m[i1][i2] = 1 + helper(s1, s2, i1+1, i2+1, m);
     else
-        m[p] = max(max(helper(s1, s2, i1+1, i2, m),
-                        helper(s1, s2, i1, i2+1, m)),
-                    helper(s1,s2,i1+1,i2+1,m));
-    return m[p];
+        m[i1][i2] = max(helper(s1, s2, i1+1, i2, m),
+                        helper(s1, s2, i1, i2+1, m));
+    return m[i1][i2];
 }
 
 int longestCommonSubsequence(string text1, string text2)
 {
-    unordered_map<pair<int,int>, int, hash_pair> m;
+    vector<vector<int>> m(text1.size(), vector<int>(text2.size()));
     return helper(text1, text2, 0, 0, m);
 }
 
-int main()
-{
-
+int longestCommonSubsequence_iter(string text1, string text2)
+{   
+    int n1 = text1.size();
+    int n2 = text2.size();
+    
+    int m[n1][n2];
+    
+    for(int i = 0; i < n1; i++)
+    {
+        for(int j = 0; j < n2; j++)
+        {
+            if(text1[i] == text2[j])
+                m[i][j] = 1 + (i-1 >= 0 && j-1 >= 0 ? m[i-1][j-1] : 0);
+            else
+            {
+                int p1 = j-1 >= 0 ? m[i][j-1] : 0;
+                int p2 = i-1 >= 0 ? m[i-1][j] : 0;
+                m[i][j] = max(p1, p2);
+            }
+        }
+    }
+    return m[n1-1][n2-1];
 }
