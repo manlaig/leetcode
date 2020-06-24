260c31b6b8c86190966d55c5264d73da7d2414e4
Michael Ganzorig <manlai1025@gmail.com>
2020-06-24 01:14:28 -0400

    a faster solution

--- group_anagrams.cpp
+++ group_anagrams.cpp
@@ -2,6 +2,30 @@
 #include <string>
 #include <vector>
 
+// this solution uses the fact that 2 anagrams are the same string when they are sorted
+// we use std::move(), which makes the function so much faster,
+// in fact it beat 95% in time and 90% in space
+std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs)
+{
+    std::unordered_map<std::string, std::vector<std::string>> m;
+    
+    for(const std::string& str : strs)
+    {
+        std::string sorted = str;
+        std::sort(sorted.begin(), sorted.end());
+        m[sorted].push_back(str);
+    }
+    
+    int i = 0;
+    std::vector<std::vector<std::string>> out(m.size());
+    for(auto it = m.begin(); it != m.end(); it++)
+        out[i++] = std::move(it->second);    // to avoid copying
+    
+    return out;
+}
+
+////////////////////////////// SLOWER SOLUTION //////////////////////////////
+// time limit exceeded :(
 bool isAnagram(std::string s1, std::string s2)
 {
     if(s1.size() != s2.size())
