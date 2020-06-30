d1eb8888e929d53098e9f0f28608b884224c50ba
Michael Ganzorig <manlai1025@gmail.com>
2020-06-30 00:51:28 -0400

    faster algorithm

--- permutation_in_string.cpp
+++ permutation_in_string.cpp
@@ -4,6 +4,48 @@ using namespace std;
 
 bool checkInclusion(const string& s1, const string& s2)
 {
+    if(s1.size() > s2.size())
+        return false;
+        
+    unordered_map<char,int> m;
+    for(int i = 0; i < s1.size(); i++)
+    {
+        m[s2[i]]++;
+        m[s1[i]]--;
+    }
+    
+    // letter occurrences that are missing
+    unordered_map<char, int> left;
+    for(pair<char, int> p : m)
+        if(p.second)
+            left[p.first] = p.second;
+    
+    if(left.empty())
+        return true;
+    
+    for(int i = 1; i + s1.size() <= s2.size(); i++)
+    {
+        left[s2[i - 1]]--;
+        if(left[s2[i - 1]] == 0)
+        {
+            left.erase(s2[i - 1]);
+        }
+
+        left[s2[i + s1.size() - 1]]++;
+        if(left[s2[i + s1.size() - 1]] == 0)
+        {
+            left.erase(s2[i + s1.size() - 1]);
+        }
+        
+        if(left.empty())
+            return true;
+    }
+    return left.empty();
+}
+
+///////////////// Time Limit Exceeded /////////////////
+bool checkInclusion_slow(const string& s1, const string& s2)
+{
     if(s2.size() < s1.size())
         return false;
     
