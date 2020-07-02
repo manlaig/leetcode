e5ad6d4db28a480f4af18edcf4d4d7a2b3dddc42
Michael Ganzorig <manlai1025@gmail.com>
2020-07-02 01:10:21 -0400

    solution

--- longest_arithmetic_sequence.cpp
+++ longest_arithmetic_sequence.cpp
@@ -1,7 +1,29 @@
 #include <unordered_map>
 #include <vector>
 
+// harder, better, faster, stronger
 int longestArithSeqLength(const std::vector<int>& A)
+{
+    // dp[i][j] = length of sequence ending at index i with difference of j
+    std::vector<std::unordered_map<int, int>> dp(A.size());
+    
+    int best = 0;
+    for(int i = 1; i < A.size(); i++)
+    {
+        for(int j = 0; j < i; j++)
+        {
+            int diff = A[i] - A[j];
+            int prev = j && dp[j][diff] ? dp[j][diff] : 1;
+            
+            dp[i][diff] = prev + 1;
+            best = std::max(best, dp[i][diff]);
+        }
+    }
+    return best;
+}
+
+// The below code caused Internal Error, who knows why
+int longestArithSeqLength_slow(const std::vector<int>& A)
 {
     // for each number, we store the last number in the sequence and the length of the sequence
     std::unordered_map<int, int> dp[A.size()];
