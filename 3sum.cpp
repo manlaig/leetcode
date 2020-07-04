79fe37571aef8c80de9da17d784f6028ae660164
Michael Ganzorig <manlai1025@gmail.com>
2020-07-04 18:37:48 -0400

    solution

--- 3sum.cpp
+++ 3sum.cpp
@@ -1,38 +1,44 @@
+#include <unordered_set>
 #include <unordered_map>
 #include <algorithm>
 #include <vector>
 #include <set>
+#include <map>
 using namespace std;
 
+// time limit exceeded
 vector<vector<int>> threeSum(vector<int>& nums)
 {
-    if(!nums.size())
-        return {};
-    unordered_map<int, list<int>> occur;
-    for(int i = 0; i < nums.size(); i++)
-        occur[nums[i]].push_back(i);
+    map<int, int> s;
+    for(int num : nums)
+        s[num]++;
     
-    set<vector<int>> unique;
-    for(int i = 0; i < nums.size()-1; i++)
+    vector<vector<int>> out;
+    for(auto it = s.begin(); it != s.end(); it++)
     {
-        for(int j = i + 1; j < nums.size(); j++)
+        if(it->second > 2 && 3 * it->first == 0)
+            out.push_back({it->first, it->first, it->first});
+        auto t1 = it;
+        advance(t1, 1);
+        for(; t1 != s.end(); t1++)
         {
-            int c = -(nums[i]+nums[j]);
-            vector<int> v = { nums[i], nums[j], c };
-            sort(v.begin(), v.end());
-            
-            for(auto iter = occur[c].begin(); iter != occur[c].end(); iter++)
-                if(*iter > i && *iter > j)
-                {
-                    unique.insert(v);
-                    break;
-                }
+            if(it->second > 1 && t1->first + 2 * it->first == 0)
+            {
+                out.push_back({it->first, it->first, t1->first});
+            }
+            if(t1->second > 1 && it->first + 2 * t1->first == 0)
+            {
+                out.push_back({it->first, t1->first, t1->first});
+            }
+            auto t2 = t1;
+            advance(t2, 1);
+            for(; t2 != s.end(); t2++)
+            {
+                if(it->first + t1->first + t2->first == 0)
+                    out.push_back({it->first, t1->first, t2->first});
+            }
         }
     }
-    vector<vector<int>> out(unique.size(), vector<int>(3, 1));
-    int i = 0;
-    for(vector<int> s : unique)
-        out[i++] = s;
     return out;
 }
 
