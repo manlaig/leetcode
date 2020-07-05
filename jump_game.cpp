f3872370a56919dc5b469345d5aa4d7322e4ccf5
Michael Ganzorig <manlai1025@gmail.com>
2020-07-05 01:55:17 -0400

    forward DP solution

--- jump_game.cpp
+++ jump_game.cpp
@@ -1,7 +1,6 @@
 #include <unordered_map>
 #include <vector>
 
-// Time limit exceeds with this solution
 bool helper(const std::vector<int>& nums, int start, std::unordered_map<int,bool>& dp)
 {
     if(dp.find(start) != dp.end())
@@ -20,18 +19,37 @@ bool helper(const std::vector<int>& nums, int start, std::unordered_map<int,bool
     return false;
 }
 
-bool canJump_recur(std::vector<int>& nums)
+// Time limit exceeds with this solution
+bool canJump_recur_DP(std::vector<int>& nums)
 {
     std::unordered_map<int,bool> dp;
     return helper(nums, 0, dp);
 }
 
-bool canJump_iter(vector<int>& nums)
+// Time limit exceeds with this solution
+bool canJump_forward_DP(const std::vector<int>& nums)
+{
+    std::vector<bool> dp(nums.size());
+    dp[0] = true;
+    for(int i = 0; i < nums.size(); i++)
+    {
+        if(dp[i])
+        {
+            if(i + nums[i] >= nums.size())
+                return true;
+            for(int j = i+1; j <= i + nums[i]; j++)
+                dp[j] = true;
+        }
+    }
+    return dp[nums.size()-1];
+}
+
+bool canJump_iter(std::vector<int>& nums)
 {
     int reachable = 0;
     for(int i = 0; i <= reachable; i++)
     {
-        reachable = max(reachable, nums[i]+i);
+        reachable = std::max(reachable, nums[i]+i);
         if(reachable >= nums.size()-1)
             return true;
     }
