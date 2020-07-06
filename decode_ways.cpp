c05c40fd970c112741497f7b2aa90c539b5452ee
Michael Ganzorig <manlai1025@gmail.com>
2020-07-06 00:06:11 -0400

    iterative DP solution

--- decode_ways.cpp
+++ decode_ways.cpp
@@ -1,6 +1,31 @@
 #include <unordered_map>
 #include <iostream>
 #include <string>
+#include <vector>
+
+int numDecodings_iter_DP(std::string s)
+{
+    int n = s.size();
+    if(n <= 0)
+        return 0;
+    std::vector<int> dp(n);
+    
+    dp[0] = s[0] != '0' ? 1 : 0;
+    for(int i = 1; i < n; i++)
+    {
+        if(s[i] == '0' && (s[i-1] != '1' && s[i-1] != '2'))
+            return 0;
+        else if(s[i-1] == '2' && s[i] <= '6' || s[i-1] == '1')
+        {
+            int one = s[i] != '0' ? dp[i-1] : 0;
+            int prev = i-2 >= 0 ? dp[i-2] : dp[i-1];
+            dp[i] = prev + one;
+        }
+        else
+            dp[i] = dp[i-1];
+    }
+    return dp[n-1];
+}
 
 /*
     This solution uses tree recursion along with dynamic programming
@@ -8,7 +33,6 @@
 
     In fact, this solution beat 100% of C++ submissions, in terms of speed.
 */
-
 int helper(const std::string& s, int start, std::unordered_map<int, int>& m)
 {
     if(start > s.size())
@@ -39,4 +63,6 @@ int main()
 {
     std::cout << numDecodings("12345678910") << std::endl;
     std::cout << numDecodings("10203040") << std::endl;
+    std::cout << numDecodings_iter_DP("12345678910") << std::endl;
+    std::cout << numDecodings_iter_DP("10203040") << std::endl;
 }
