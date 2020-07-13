f56fb948648c729a7364f05e6c2565fcb75bc02a
Michael Ganzorig <manlai1025@gmail.com>
2020-07-13 18:52:45 -0400

    solution

---
+++ palindrome_partitioning.cpp
@@ -0,0 +1,63 @@
+#include <unordered_map>
+#include <vector>
+#include <string>
+using namespace std;
+
+void palindrome_odd(unordered_map<int, vector<int>>& dp, string s, int i)
+{
+    dp[i].push_back(i+1);
+    int left = i-1;
+    int right = i+1;
+    while(left >= 0 && right < s.size() && s[left] == s[right])
+    {
+        dp[left].push_back(right+1);
+        left--;
+        right++;
+    }
+}
+
+void palindrome_even(unordered_map<int, vector<int>>& dp, string s, int i)
+{
+    int left = i;
+    int right = i+1;
+    while(left >= 0 && right < s.size() && s[left] == s[right])
+    {
+        dp[left].push_back(right+1);
+        left--;
+        right++;
+    }
+}
+
+void fill(string s, vector<vector<string>>& out, unordered_map<int, vector<int>>& dp, int i, vector<string> next)
+{
+    if(i >= s.size())
+    {
+        out.push_back(next);
+        return;
+    }
+    for(auto end : dp[i])
+    {
+        next.push_back(s.substr(i, end-i));
+        fill(s, out, dp, end, next);
+        next.pop_back();
+    }
+}
+
+vector<vector<string>> partition(string s)
+{
+    // dp[i] = all the indices where the palindrome at i ends
+    unordered_map<int, vector<int>> dp;
+    for(int i = 0; i < s.size(); i++)
+    {
+        palindrome_odd(dp, s, i);
+        palindrome_even(dp, s, i);
+    }
+    vector<vector<string>> out;
+    fill(s, out, dp, 0, {});
+    return out;
+}
+
+int main()
+{
+
+}
