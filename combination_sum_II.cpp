c821cef3ae83af82d6f9e628f4c9f70337fd4394
Michael Ganzorig <manlai1025@gmail.com>
2020-07-24 18:27:39 -0400

    solution explanation

--- combination_sum_II.cpp
+++ combination_sum_II.cpp
@@ -2,13 +2,14 @@
 #include <algorithm>
 using namespace std;
 
-void dfs(const vector<int>& arr, vector<vector<int>>& out, vector<int> next, int left, int curr)
+void dfs(const vector<int>& arr, vector<vector<int>>& out, vector<int>& next, int left, int curr)
 {
     if(left == 0)
     {
         out.push_back(next);
     }
     
+    // +1 to avoid using the same element more than once
     for(int i = curr+1; i < arr.size(); i++)
     {
         if(left - arr[i] >= 0)
@@ -16,6 +17,9 @@ void dfs(const vector<int>& arr, vector<vector<int>>& out, vector<int> next, int
             next.push_back(arr[i]);
             dfs(arr, out, next, left-arr[i], i);
             next.pop_back();
+            
+            // the duplicate elements are already used by
+            // recursive calls, so skip them here
             while(i+1 < arr.size() && arr[i] == arr[i+1])
                 i++;
         }
@@ -26,7 +30,8 @@ vector<vector<int>> combinationSum2(vector<int>& candidates, int target)
 {
     sort(candidates.begin(), candidates.end());
     vector<vector<int>> out;
-    dfs(candidates, out, {}, target, -1);
+    vector<int> tmp;
+    dfs(candidates, out, tmp, target, -1);
     return out;
 }
 
