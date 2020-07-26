7764048055dce32f23a12b35f0b16db3b70db098
Michael Ganzorig <manlai1025@gmail.com>
2020-07-26 00:59:44 -0400

    remove class

--- valid_parantheses.cpp
+++ valid_parantheses.cpp
@@ -1,30 +1,26 @@
 #include <stack>
 #include <string>
 
-class Solution
+bool isValid(std::string s)
 {
-public:
-    bool isValid(std::string s)
+    std::stack<char> p;
+    for(int i = 0; i < s.size(); i++)
     {
-        std::stack<char> p;
-        for(int i = 0; i < s.size(); i++)
-        {
-            if(s[i] == '{' || s[i] == '[' || s[i] == '(')
-                p.push(s[i]);
-            else if(p.empty())
-                return false;
-            else if(s[i] == '}' && p.top() != '{')
-                return false;
-            else if(s[i] == ')' && p.top() != '(')
-                return false;
-            else if(s[i] == ']' && p.top() != '[')
-                return false;
-            else
-                p.pop();
-        }
-        return p.empty();
+        if(s[i] == '{' || s[i] == '[' || s[i] == '(')
+            p.push(s[i]);
+        else if(p.empty())
+            return false;
+        else if(s[i] == '}' && p.top() != '{')
+            return false;
+        else if(s[i] == ')' && p.top() != '(')
+            return false;
+        else if(s[i] == ']' && p.top() != '[')
+            return false;
+        else
+            p.pop();
     }
-};
+    return p.empty();
+}
 
 int main()
 {
