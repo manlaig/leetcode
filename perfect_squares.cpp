3922d8d1463d14f8fc6c66d7868353a9fe83293e
Michael Ganzorig <manlai1025@gmail.com>
2020-07-29 01:19:50 -0400

    explanation

--- perfect_squares.cpp
+++ perfect_squares.cpp
@@ -5,11 +5,15 @@ using namespace std;
 int numSquares(int n)
 {
     int k = sqrt(n);
+    
+    // dp[n][k] = min number of squares up to k that add up to n
     vector<vector<int>> dp(n+1, vector<int>(k+1));
     
     for(int i = 1; i <= n; i++)
         for(int j = 1; j <= k; j++)
         {
+            // INT_MAX to force the use of 1 in dp[i][j]
+            // when j == 1, the else clause will be run
             int t = j == 1 ? INT_MAX : dp[i][j-1];
             if(i - j*j < 0)
                 dp[i][j] = t;
