fbb2ebc29312e5eac710b508a2d7a89ecd38502f
Michael Ganzorig <manlai1025@gmail.com>
2020-07-29 01:08:04 -0400

    faster solution

--- longest_valid_parantheses.cpp
+++ longest_valid_parantheses.cpp
@@ -2,6 +2,44 @@
 
 int longestValidParentheses(const std::string& s)
 {
+    int curr = 0;
+    int start = 0;
+    int out = 0;
+    int peak = 0;
+    
+    for(int i = 0; i < s.size(); i++)
+    {
+        if(s[i] == '(')
+        {
+            curr++;
+            peak++;
+        }
+        else
+        {
+            curr--;
+            if(curr > 0)
+                out = std::max(out, (peak - curr)*2);
+            peak--;
+        }
+        
+        if(curr < 0)
+        {
+            start = i+1;
+            peak = 0;
+            curr = 0;
+        }
+        else if(curr == 0)
+        {
+            out = std::max(out, i - start + 1);
+            peak = 0;
+        }
+    }
+    return out;
+}
+
+// time limit exceeded
+int longestValidParentheses(const std::string& s)
+{
     if(!s.size())
         return 0;
     
