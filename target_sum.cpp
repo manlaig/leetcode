39a3c28ac3873eac3bc2178d62cb9a9a4b9bf2a9
Michael Ganzorig <manlai1025@gmail.com>
2020-07-31 17:37:47 -0400

    another solution

--- target_sum.cpp
+++ target_sum.cpp
@@ -2,6 +2,43 @@
 #include <vector>
 using namespace std;
 
+// O(1000*N) algorithm in the ideal worst case
+int findTargetSumWays(vector<int>& nums, int S)
+{
+    if(!nums.size())
+        return 0;
+    
+    int total = 0;
+    int largest = 0;
+    for(int num : nums)
+    {
+        total += num;
+        largest = max(largest, num);
+    }
+    
+    if(S > 1000 || (S + total) % 2)
+        return 0;
+
+    int target = (S + total) / 2;
+    
+    vector<vector<int>> dp(nums.size(), vector<int>(max(target, largest)+1));
+    dp[0][0] = 1;
+    dp[0][nums[0]] += 1;    // += is needed here if nums[0] = 0
+    // this is because you can choose -0 and +0 and get 2 different 0s
+    
+    for(int i = 1; i < nums.size(); i++)
+    {
+        for(int j = 0; j <= target; j++)
+        {
+            if(j - nums[i] >= 0)
+                dp[i][j] = dp[i-1][j - nums[i]] + dp[i-1][j];
+            else
+                dp[i][j] = dp[i-1][j];
+        }
+    }
+    return dp[nums.size()-1][target];
+}
+
 // O(N * 2000) algorithm 
 int findTargetSumWays(vector<int>& nums, int S)
 {
