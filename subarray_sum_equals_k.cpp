80d8ea7d5bee080822ae5ac93f6fa75fdfb15f90
Michael Ganzorig <manlai1025@gmail.com>
2020-07-31 22:51:23 -0400

    explanation of solution

--- subarray_sum_equals_k.cpp
+++ subarray_sum_equals_k.cpp
@@ -1,6 +1,12 @@
 #include <unordered_map>
 #include <vector>
 
+/*
+    The idea behind this approach is that if we have an array
+    containing sum of all elements up to i, then a continuous
+    subarray with sum k ending at index j means it has a difference
+    of k between index i and j in the array of sums. 
+*/
 int subarraySum(const std::vector<int>& nums, int k)
 {
     std::unordered_map<int, int> lookup;
