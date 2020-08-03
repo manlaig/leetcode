1d8ba3796d8554832d99238ea7a57564d4789304
Michael Ganzorig <manlai1025@gmail.com>
2020-08-02 23:35:54 -0400

    more space efficient solution

--- house_robber.cpp
+++ house_robber.cpp
@@ -1,6 +1,35 @@
 #include <vector>
 
+// O(N) time and O(1) space
 int rob(const std::vector<int>& nums)
+{
+    if(!nums.size())
+        return 0;
+    if(nums.size() == 1)
+        return nums[0];
+    if(nums.size() == 2)
+        return std::max(nums[0], nums[1]);
+    if(nums.size() == 3)
+        return std::max(nums[2] + nums[0], nums[1]);
+
+    int one = nums[0];
+    int two = nums[1];
+    int three = nums[2] + one;
+    int four = nums[3] + std::max(two, one);
+    
+    for(int i = 4; i < nums.size(); i++)
+    {
+        int temp = four;
+        four = nums[i] + std::max(three, two);
+        one = two;
+        two = three;
+        three = temp;
+    }
+    
+    return std::max(four, three);
+}
+
+int rob_DP(const std::vector<int>& nums)
 {   
     if(!nums.size())
         return 0;
