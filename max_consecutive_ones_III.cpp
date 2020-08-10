4ad51dfc16907e4d5b8e8b71a6fd639b17a3a5a3
Michael Ganzorig <manlai1025@gmail.com>
2020-08-09 23:57:50 -0400

    solution

--- max_consecutive_ones_III.cpp
+++ max_consecutive_ones_III.cpp
@@ -1,4 +1,3 @@
-#include <unordered_map>
 #include <vector>
 using namespace std;
 
@@ -34,29 +33,6 @@ int longestOnes(vector<int>& A, int K)
         }
     }
     return A.size() - start;
-}
-
-/////////////////// slow solution //////////////////
-int helper(const vector<int>& arr, int start, int k, int curr = 0)
-{
-    if(start >= arr.size())
-    {
-        return curr;
-    }
-    if(arr[start] == 1)
-    {
-        return helper(arr, start+1, k, curr+1);
-    }
-    if(k > 0)
-    {
-        return max(helper(arr, start+1, k-1, curr+1), helper(arr, start+1, k));
-    }
-    return max(curr, helper(arr, start+1, k, 0));
-}
-
-int longestOnes_slow(vector<int>& A, int K)
-{
-    return helper(A, 0, K);
 }
 
 int main()
