b4c23edd02f869259ad0488b0c14d893bc53b306
Michael Ganzorig <manlai1025@gmail.com>
2020-08-10 00:10:51 -0400

    explanation

--- daily_temperatures.cpp
+++ daily_temperatures.cpp
@@ -2,6 +2,7 @@
 #include <stack>
 using namespace std;
 
+// There's similarity between this solution and the Longest Valid Parantheses solution
 std::vector<int> dailyTemperatures(const std::vector<int>& T)
 {
     int n = T.size()-1;
@@ -9,10 +10,18 @@ std::vector<int> dailyTemperatures(const std::vector<int>& T)
     s.push({n, T[n]});
     
     std::vector<int> out(n+1);
+
+    // We start looping from the right because at index i
+    // we'll have already visited indices larger than i (can reuse)
     for(int i = n; i >= 0; i--)
     {
+        // we don't care about elements smaller than T[i]
+        // because we don't need them since indices 0 <= j < i
+        // are either smaller than T[i] (in which case out[j] = i-j)
+        // or larger than T[i] (in which case T[i] will be popped)
         while(s.size() && s.top().second <= T[i])
             s.pop();
+        
         if(!s.empty())
             out[i] = s.top().first - i;
         s.push({i, T[i]});
