b0172951b28f7deb9441aca7900093918fabd9c7
Michael Ganzorig <manlai1025@gmail.com>
2020-08-11 01:10:52 -0400

    explanation of solution

--- queue_reconstruction_by_height.cpp
+++ queue_reconstruction_by_height.cpp
@@ -2,25 +2,37 @@
 #include <vector>
 using namespace std;
 
+/*
+    An important observation to make here is that the shortest person's spot is already given.
+    Because everyone else will be taller than them, people[i][1] is the spot for the shortest person at i
+    The next taller person skips enough taller people (elements initialized to INT_MAX) and pick the next available spot
+*/
+
 vector<vector<int>> reconstructQueue(vector<vector<int>>& people)
 {
+    // start with the shortest person
     sort(people.begin(), people.end());
     
+    // to avoid overriding
     vector<bool> filled(people.size());
     vector<vector<int>> out(people.size(), {INT_MAX, INT_MAX});
     for(int i = 0; i < people.size(); i++)
     {
+        // counting how many taller people encountered so far
         int count = 0;
-        int j = 0;
-        for(; j < out.size(); j++)
+        int spot = 0;
+        for(; spot < out.size(); spot++)
         {
-            if(count >= people[i][1] && !filled[j])
+            // if we skipped enough taller people and the spot is empty
+            if(count >= people[i][1] && !filled[spot])
                 break;
-            if(people[i][0] <= out[j][0])
+
+            // need to skip exactly people[i][1] taller people
+            if(people[i][0] <= out[spot][0])
                 count++;
         }
-        out[j] = people[i];
-        filled[j] = true;
+        out[spot] = people[i];
+        filled[spot] = true;
     }
     return out;
 }
