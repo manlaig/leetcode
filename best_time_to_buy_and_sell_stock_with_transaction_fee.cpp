793178c6f002e655b3e073cd6c2468731a6ab087
Michael Ganzorig <manlai1025@gmail.com>
2020-08-12 12:15:08 -0400

    explanation of solution

--- best_time_to_buy_and_sell_stock_with_transaction_fee.cpp
+++ best_time_to_buy_and_sell_stock_with_transaction_fee.cpp
@@ -12,9 +12,14 @@ int maxProfit(vector<int>& prices, int fee)
     
     for(int i = 1; i < prices.size(); i++)
     {
+        // propagating the maximum profit
         dp[i][1] = dp[i-1][1];
+        // there can be a new max profit on this day
         if(dp[i-1][0] + prices[i] > 0)
-            dp[i][1] = max(dp[i-1][1], dp[i-1][0] + prices[i]);
+            dp[i][1] = max(dp[i][1], dp[i-1][0] + prices[i]);
+
+        // either the buy transaction can propagate on this day
+        // or we can get a cheaper buy instead
         dp[i][0] = max(dp[i-1][0], -prices[i] - fee + dp[i-1][1]);
     }
     
