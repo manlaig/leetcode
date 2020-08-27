92273fbc9b780185cb7f90c1f51a795e3ab58864
Michael Ganzorig <manlai1025@gmail.com>
2020-08-26 18:45:17 -0700

    explanation of solution

--- task_scheduler.cpp
+++ task_scheduler.cpp
@@ -1,22 +1,34 @@
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 using namespace std;
+
+/*
+    This solution can be solved using a greedy approach
+    Then, the question is "what" should be pick greedily?
+    Few options are:
+        1. Earliest/Latest executed tasks
+        2. A task that has the most tasks remaining to execute
+    
+    Option 1 leads to some tasks finish execute earlier and leaves with unnecesssary idles
+    Option 2 asks what is the task that can be executed at this point that has the most tasks remaining
+
+    Option 2 avoids finishing some tasks earlier than expected, thus saving idle time for the CPU
+*/
 
 // a relatively slow solution
 int leastInterval(const vector<char>& tasks, int n)
 {
     unordered_map<char, int> occur;
-    for(char c : tasks)
-        occur[c]++;
-    
-    vector<char> order(occur.size());
-    int index = 0;
-    for(const pair<char, int>& p : occur)
-        order[index++] = p.first;
-            
+    // last[c]  = time when task 'c' was last executed
     unordered_map<char, int> last;
-    for(char c : order)
+    unordered_set<char> unique;
+    for(char c : tasks)
+    {
+        occur[c]++;
         last[c] = -1;
+        unique.insert(c);
+    }
     
     int count = 0;
     int done = 0;
@@ -25,7 +37,7 @@ int leastInterval(const vector<char>& tasks, int n)
         char greedy = 0;
         int lastGreedy = 0;
         
-        for(char c : order)
+        for(char c : unique)
         {
             if((last[c] == -1 || count - last[c] > n) && occur[c] > lastGreedy)
             {
