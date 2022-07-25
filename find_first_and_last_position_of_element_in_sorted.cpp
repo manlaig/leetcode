64a95833a8a853563a05f7bd4016c006f23d8bf1
Michael Ganzorig <manlai1025@gmail.com>
2022-07-24 22:25:40 -0700

    Add another approach

--- find_first_and_last_position_of_element_in_sorted.cpp
+++ find_first_and_last_position_of_element_in_sorted.cpp
@@ -41,6 +41,42 @@ std::vector<int> searchRange(const std::vector<int>& nums, int target)
     return { getStart(nums, target, 0, nums.size()-1), getEnd(nums, target, 0, nums.size()-1) };
 }
 
+///////////////////////////// Second approach ///////////////////////////////
+int binarySearch(const vector<int>& nums, int target, int start, int end)
+{
+    if(start >= end)
+        return -1;
+    
+    int mid = start + (end - start) / 2;
+
+    if(nums[mid] == target)
+        return mid;
+    else if(nums[mid] > target)
+        return binarySearch(nums, target, start, mid);
+    return binarySearch(nums, target, mid + 1, end);
+}
+
+vector<int> searchRange2(vector<int>& nums, int target)
+{
+    if(!nums.size())
+        return {-1, -1};
+    int index = binarySearch(nums, target, 0, nums.size());
+    cout << index << endl;
+    if(index == -1)
+        return {-1, -1};
+    
+    int left = index;
+    int right = index;
+    while(left >= 0 && nums[left] == target)
+        left--;
+    
+    while(right < nums.size() && nums[right] == target)
+        right++;
+    
+    return {left + 1, right - 1};
+}
+//////////////////////////////////////////////////////////////////////////////z
+
 int main()
 {
 
